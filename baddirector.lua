f6e1aaaa6cfbff588d070c98b93e2dd155e215af
Clicky- <84652396+Clickseee@users.noreply.github.com>
2025-09-21 20:31:30 +0700

    A new beninging

---
+++ baddirector.lua
@@ -0,0 +1,10 @@
+--#region MISC STUFF
+
+BadDirector = SMODS.current_mod
+
+local path = SMODS.current_mod.path .. 'jokers/'
+for _, v in pairs(NFS.getDirectoryItems(path)) do
+    assert(SMODS.load_file('jokers/' .. v))()
+end
+
+--#endregion
