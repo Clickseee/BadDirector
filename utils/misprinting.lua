0b4aa0d8bb172448b017b7cd2e06439a5a7ec772
Ruby <138878489+lord-ruby@users.noreply.github.com>
2026-01-24 21:48:02 +0000

    black hole fixes

--- utils/misprinting.lua
+++ utils/misprinting.lua
@@ -151,101 +151,49 @@ function BadDirector.misprint_hand(hand, card, seed)
     })
 end
 
-function BadDirector.misprint_hand(hand, card, seed)
-    seed = seed or hand..card.config.center.key
-    update_hand_text({delay = 0}, {handname = localize(hand, "poker_hands"), 
+function BadDirector.misprint_all(card, seed)
+    seed = seed or card.config.center.key
+    update_hand_text({delay = 0}, {handname = localize('k_all_hands'), 
     mult = "...", chips = "..."})
     delay(1)
-    if SMODS.pseudorandom_probability(card, seed, 1, 4) then
-        for hand, v in pairs(G.GAME.hands) do
+    for hand, v in pairs(G.GAME.hands) do
+        if SMODS.pseudorandom_probability(card, seed, 1, 4) then
             local range = math.floor((pseudorandom(seed)*1.3 + 0.7) * 100) / 100
             local range2 = math.floor((pseudorandom(seed)*1.3 + 0.7) * 100) / 100
             G.GAME.hands[hand].mult = G.GAME.hands[hand].mult * range
             G.GAME.hands[hand].chips = G.GAME.hands[hand].chips * range2
-        end
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2, func = function()
-            play_sound('bd_inapmit_fast')
-            if card and card.juice_up then card:juice_up(0.8, 0.5) end
-            G.TAROT_INTERRUPT_PULSE = true
-            return true end }))
-        update_hand_text({delay = 0}, {mult = "X???", StatusText = true})
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('bd_inapmit_fast')
-            if card and card.juice_up then card:juice_up(0.8, 0.5) end
-            return true end }))
-            G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('bd_inapmit_fast')
-            if card and card.juice_up then card:juice_up(0.8, 0.5) end
-            return true end }))
-        update_hand_text({delay = 0}, {handname = localize(hand, "poker_hands"), level = G.GAME.hands[hand].level, 
-            mult = G.GAME.hands[hand].mult, chips = G.GAME.hands[hand].chips, hand})
-        update_hand_text({delay = 0}, {chips = "X???", StatusText = true})
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('bd_inapmit_fast')
-            if card and card.juice_up then card:juice_up(0.8, 0.5) end
-            G.TAROT_INTERRUPT_PULSE = nil
-            return true end }))
-        update_hand_text({delay = 0}, {handname = localize(hand, "poker_hands"), level = G.GAME.hands[hand].level, 
-            mult = G.GAME.hands[hand].mult, chips = G.GAME.hands[hand].chips, hand})
-        update_hand_text({sound = 'button', volume = 0.7, pitch = 0.9, delay = 0}, {level=G.GAME.hands[hand].level})
-        delay(1.3)
-    elseif SMODS.pseudorandom_probability(card, seed, 1, 2) then
-        for hand, v in pairs(G.GAME.hands) do
+        elseif SMODS.pseudorandom_probability(card, seed, 1, 2) then
             local range = math.floor((pseudorandom(seed)*49 + 1) * 100) / 100
             local range2 = math.floor((pseudorandom(seed)*49 + 1) * 100) / 100
             G.GAME.hands[hand].mult = G.GAME.hands[hand].mult + range
             G.GAME.hands[hand].chips = G.GAME.hands[hand].chips + range2
-        end
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2, func = function()
-            play_sound('bd_inapmit_fast')
-            if card and card.juice_up then card:juice_up(0.8, 0.5) end
-            G.TAROT_INTERRUPT_PULSE = true
-            return true end }))
-        update_hand_text({delay = 0}, {mult = "+???", StatusText = true})
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('bd_inapmit_fast')
-            if card and card.juice_up then card:juice_up(0.8, 0.5) end
-            return true end }))
-            G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('bd_inapmit_fast')
-            if card and card.juice_up then card:juice_up(0.8, 0.5) end
-            return true end }))
-        update_hand_text({delay = 0}, {chips = "+???", StatusText = true})
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('bd_inapmit_fast')
-            if card and card.juice_up then card:juice_up(0.8, 0.5) end
-            G.TAROT_INTERRUPT_PULSE = nil
-            return true end }))
-        update_hand_text({sound = 'button', volume = 0.7, pitch = 0.9, delay = 0}, {level=G.GAME.hands[hand].level})
-        delay(1.3)
-    else    
-        for hand, v in pairs(G.GAME.hands) do
+        else
             G.GAME.hands[hand].mult = G.GAME.hands[hand].mult + 0.5
             G.GAME.hands[hand].chips = G.GAME.hands[hand].chips + 0.5
         end
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2, func = function()
-            play_sound('bd_inapmit_fast')
-            if card and card.juice_up then card:juice_up(0.8, 0.5) end
-            G.TAROT_INTERRUPT_PULSE = true
-            return true end }))
-        update_hand_text({delay = 0}, {mult = "+0.5", StatusText = true})
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('bd_inapmit_fast')
-            if card and card.juice_up then card:juice_up(0.8, 0.5) end
-            return true end }))
-            G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('bd_inapmit_fast')
-            if card and card.juice_up then card:juice_up(0.8, 0.5) end
-            return true end }))
-        update_hand_text({delay = 0}, {chips = "+0.5", StatusText = true})
-        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('bd_inapmit_fast')
-            if card and card.juice_up then card:juice_up(0.8, 0.5) end
-            G.TAROT_INTERRUPT_PULSE = nil
-            return true end }))
-        update_hand_text({sound = 'button', volume = 0.7, pitch = 0.9, delay = 0}, {level=G.GAME.hands[hand].level})
-        delay(1.3)
     end
+    G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2, func = function()
+        play_sound('bd_inapmit_fast')
+        if card and card.juice_up then card:juice_up(0.8, 0.5) end
+        G.TAROT_INTERRUPT_PULSE = true
+        return true end }))
+    update_hand_text({delay = 0}, {mult = "???", StatusText = true})
+    G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
+        play_sound('bd_inapmit_fast')
+        if card and card.juice_up then card:juice_up(0.8, 0.5) end
+        return true end }))
+        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
+        play_sound('bd_inapmit_fast')
+        if card and card.juice_up then card:juice_up(0.8, 0.5) end
+        return true end }))
+    update_hand_text({delay = 0}, {chips = "???", StatusText = true})
+    G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
+        play_sound('bd_inapmit_fast')
+        if card and card.juice_up then card:juice_up(0.8, 0.5) end
+        G.TAROT_INTERRUPT_PULSE = nil
+        return true end }))
+    update_hand_text({sound = 'button', volume = 0.7, pitch = 0.9, delay = 0}, {level="???"})
+    delay(1.3)
     update_hand_text({ delay = 0 }, {
         mult = 0,
         chips = 0,
