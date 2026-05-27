42c11d59905d4d7c4dca8d83c81de03343a982ca
Nix767 <nixbrooks767@gmail.com>
2026-05-26 22:33:30 -0400

    The Ache no longer creates "duplicates" of Hearts
    it used to do that but they'd disappear on going back to menu lol

--- other/blinds.lua
+++ other/blinds.lua
@@ -23,12 +23,22 @@ SMODS.Blind {
                 end
             end
         end
-        if context.end_of_round and context.main_eval then
+        if context.end_of_round and context.main_eval and not blind.disabled then
             if G.GAME.ache_capture and #G.GAME.ache_capture > 0 then
                 for i=1, #G.GAME.ache_capture do
-                    local heart = BadDirector.copy_card(G.GAME.ache_capture[i], nil, G.hand)
-                    heart:add_to_deck()
-                    table.insert(G.playing_cards, heart)
+                    G.playing_card = (G.playing_card and G.playing_card + 1) or 1
+                    local hearty = copy_card(G.GAME.ache_capture[i], nil, nil, G.playing_card)
+                    hearty:add_to_deck()
+                    G.deck.config.card_limit = G.deck.config.card_limit + 1
+                    table.insert(G.playing_cards, hearty)
+                    G.deck:emplace(hearty)
+                    hearty.states.visible = nil
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            hearty:start_materialize()
+                            return true
+                        end
+                    }))
                 end
             end
 		end
@@ -36,9 +46,19 @@ SMODS.Blind {
     disable = function(self)
         if G.GAME.ache_capture and #G.GAME.ache_capture > 0 then
             for i=1, #G.GAME.ache_capture do
-                local heart = BadDirector.copy_card(G.GAME.ache_capture[i], nil, G.hand)
+                G.playing_card = (G.playing_card and G.playing_card + 1) or 1
+                local hearty = copy_card(G.GAME.ache_capture[i], nil, nil, G.playing_card)
+                hearty:add_to_deck()
                 G.deck.config.card_limit = G.deck.config.card_limit + 1
-                table.insert(G.playing_cards, heart)
+                table.insert(G.playing_cards, hearty)
+                G.deck:emplace(hearty)
+                hearty.states.visible = nil
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        hearty:start_materialize()
+                        return true
+                    end
+                }))
             end
             G.GAME.ache_capture = {}
         end
