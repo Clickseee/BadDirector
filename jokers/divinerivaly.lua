3855cf94a7442b989d83d138d5c2f1c029994c1e
Foo45 <rowan.stjames.cooper@gmail.com>
2026-06-05 10:41:24 -0400

    Add: Divine Rivaly ( needs art )

---
+++ jokers/divinerivaly.lua
@@ -0,0 +1,34 @@
+SMODS.Atlas{
+	key = 'MACHINE I WILL STRIKE YOU DOWN',
+	path = "divine rivaly.png",
+	px = 1919,
+	py = 1079
+}
+
+SMODS.Joker{
+	key = "divine_rivaly",
+	atlas = "MACHINE I WILL STRIKE YOU DOWN",
+	rarity = 2,
+	cost = 7,
+	pools = {
+		["BadDirector_Jokers"] = true,
+	},
+	coder = {"Foo54"},
+	attributes = {"boss blind"},
+	blueprint_compat = false,
+	eternal_compat = true,
+	perishable_compat = true,
+	calculate = function(self, card, context)
+		if context.debuffed_hand or context.joker_main then
+			if G.GAME.blind.triggered then
+				G.GAME.perscribed_bosses = G.GAME.perscribed_bosses or {}
+				if not G.GAME.perscribed_bosses[G.GAME.round_resets.ante + 1] then
+					G.GAME.perscribed_bosses[G.GAME.round_resets.ante + 1] = G.GAME.blind.config.blind.key
+					return {
+						message = localize("ph_bd_divine_rivaly")
+					}
+				end
+			end
+		end
+	end,
+}
--- localization/en-us.lua
+++ localization/en-us.lua
@@ -623,6 +623,13 @@ return {
                     "with every other Joker"
                 },
             },
+            j_bd_divine_rivaly = {
+                name = "Divine Rivaly",
+                text = {
+                    "Triggering the {C:attention}Boss Blind's{} ability",
+                    "will force a {C:attention}rematch{} next round"
+                }
+            },
             -- Credit Localization
             j_bd_nxkoojoker = {
                 name = "Nxkoo",
@@ -1435,7 +1442,8 @@ return {
             k_bd_celestial_pack = "estial PackCel",
             k_bd_spectral_pack = "tral PackSpec",
             k_bd_blueprint_seal = "Consumable?", -- can be adjusted as need be lmao
-            k_bd_purpleprint_seal = "Enhanced?"  -- ditto
+            k_bd_purpleprint_seal = "Enhanced?",  -- ditto
+            ph_bd_divine_rivaly = "We will meet again, machine." -- change this to what you see fit
         },
         labels = {
             bd_thermal = "Thermal",
